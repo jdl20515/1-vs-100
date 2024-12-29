a9c903fd330493c623599748c09f021a4af47496
jdl20515 <jdavidlomelin@gmail.com>
2024-12-29 17:49:09 -0500

    updates

--- main.py
+++ main.py
@@ -76,12 +76,11 @@ class Game:
         pg.display.set_caption(f'DESTORY THE GRINCH')
 
     def draw(self):
-        # self.screen.fill('black')
+        self.screen.fill('black')
         self.object_renderer.draw()
         self.weapon.draw()
-
         # self.map.draw()
-        # self.player.draw()
+        # self.player.dradw()
 
     def check_events(self):
         self.global_trigger = False
